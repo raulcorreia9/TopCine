767d8322a2cb25e98c162ab295586214d096f14e
Raul Correia <raulvinicius245@gmail.com>
2022-08-23 23:06:55 -0300

    Estrutura inicial

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 //Router
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//Pages Components
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import Search from './pages/Search';
 
+import "./index.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/:id" element={<Movie />} />
+          <Route path="/search" element={<Search />} />
+        </Route>
       </Routes>
     </BrowserRouter>
     <App />
