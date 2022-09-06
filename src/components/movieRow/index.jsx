cba60dee7f8e1fad8347765461258d38f5744635
Raul Correia <raulvinicius245@gmail.com>
2022-09-05 21:54:16 -0300

    Scroll de generos para a esquerda

--- src/components/movieRow/index.jsx
+++ src/components/movieRow/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 
 import './styles.scss';
@@ -7,18 +8,32 @@ import './styles.scss';
 const MovieRow = ({title, items}) => {
 
     const imageURL = import.meta.env.VITE_GENRES_IMG;
+
+    const [scrollX, setScrollX] = useState(-550);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + 200;
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
     return (
         <>
             <div className='movieRow'>
                 <h2 className='movieRowTitle'>{ title }</h2>
-                <div className="movieRowLeft">
+                <div className="movieRowLeft" onClick={ handleLeftArrow }>
                     <BsChevronLeft style={{ fontSize: 30 }} />
                 </div>
                 <div className="movieRowRight">
                     <BsChevronRight style={{ fontSize: 30 }} />
                 </div>
                 <div className="movieRowListArea">
-                    <div className="movieRowList">
+                    <div className="movieRowList" style={{
+                        marginLeft: scrollX,
+                        width: items.length * 200
+                    }}>
                         { items.length > 0 && items.map((item, key) => (
                             <div className="movieRowItem" key={ key }>
                                 <img src={`${imageURL}${item.poster_path}`} alt={`${ item.original_title }`} />
