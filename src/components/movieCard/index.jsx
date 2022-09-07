4de3af8467f0e17c17ae72f30650990999b17996
Raul Correia <raulvinicius245@gmail.com>
2022-09-06 21:51:00 -0300

    Responsividade para a tela de top 20 filmes

--- src/components/movieCard/index.jsx
+++ src/components/movieCard/index.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import { FaStar } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-import './styles.scss';
+import "./styles.scss";
 
 const imageURL = import.meta.env.VITE_IMG;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const MovieCard = ({ movie, showLink = true }) => {
+
   return (
     <>
-        <div className='movieCard'>
-            <img src={ imageURL + "/" + movie.poster_path } alt={ movie.original_title } />
-            <h2>{ movie.title }</h2>
-            <p>
-                <FaStar /> { movie.vote_average }
-            </p>
-            { showLink && <Link to={`/movie/${ movie.id }`} className="detailsButton">Detalhes</Link> }
+      <div className="movieCard">
+        <img 
+        className="movieImg"
+          src={imageURL + "/" + movie.poster_path}
+          alt={movie.original_title}
+        />
+        <div className="movieBasicInfoContainer">
+          <p className="movieTitle">{movie.title}</p>
+          <p className="movieStarIcon">
+            <FaStar /> {movie.vote_average}
+          </p>
         </div>
+        {showLink && (
+          <Link to={`/movie/${movie.id}`} className="detailsButton">
+            Detalhes
+          </Link>
+        )}
+      </div>
     </>
   );
-}
+};
 
-export default MovieCard
+export default MovieCard;
